260ef26fbdbf5a73fd8971509cb43ba379386e88
Dmitriy Vinokurov <m1zzo1889@gmail.com>
2022-02-28 09:32:12 +0400

    qwe

--- syncer/Program.cs
+++ syncer/Program.cs
@@ -1,15 +1,21 @@
 ﻿using syncer;
+using System.Diagnostics;
 
 namespace HelloWorld
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
 
+        private const uint ENABLE_EXTENDED_FLAGS = 0x0080;
         static async Task Main()
         {
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+            SetConsoleMode(handle, ENABLE_EXTENDED_FLAGS);
             while (true)
             {
-                List<Task> tasks = new List<Task> { Users.Firms(), Users.Firms_SelfEmployed(), Projects.DrxSync() };
+                List<Task> tasks = new List<Task> { Users.Firms(), Users.Firms_SelfEmployed()/*, Projects.DrxSync()*/ };
                 await Task.WhenAll(tasks);
                 await Projects.Sync();
                 Console.WriteLine(DateTime.Now + "   Sleep for hour");
